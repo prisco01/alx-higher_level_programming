1c8e83794ef26d50648f8da604c2ddd36fd9ab06
prisco01 <dapo.olagunju@gmail.com>
2023-03-12 02:34:51 +0000

    a program that prints alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(i), end='')
+def print_low_alpha():
+
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        print('{}'.format(c), end='')
+
+
+if __name__ == '__main__':
+    print_low_alpha()
+
+    
